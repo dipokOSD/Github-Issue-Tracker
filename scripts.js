a0369b52f943ab56062664129455c36555609197
dipokOSD <ddeb07425@gmail.com>
2026-04-11 00:56:00 +0600

    caeat a login page and functionaliti

---
+++ scripts.js
@@ -0,0 +1,13 @@
+const loginBtn=document.getElementById("login-btn")
+loginBtn.addEventListener("click",()=>{
+    const username=document.getElementById("username").value;
+    const password=document.getElementById("password").value;
+    
+    if(username==="admin" && password==="admin123"){
+        alert("Login Success")
+        window.location.href = "main.html";
+    }
+    else {
+        alert("Wrong Requared");
+    }
+})
